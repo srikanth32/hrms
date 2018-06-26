ad0dacee18037244fc09603ea992680c3910e326
Srikanth <39333529+srikanth32@users.noreply.github.com>
2018-06-26 16:23:00 +0530

    commit

--- src/go/personalDetails.go
+++ src/go/personalDetails.go
@@ -21,12 +21,12 @@ type RegisterData struct {
 	FathersName    string `json:"fathersname,omitempty"`
 	DOB            string `json:"dob,omitempty"`
 	Gender         string `json:"gender,omitempty"`
-	Phone          int    `json:"phone,string,omitempty"`
+	Phone          string `json:"phone,omitempty"`
 	Add            string `json:"add,omitempty"`
 	Nationality    string `json:"nationality,omitempty"`
 	Status         string `json:"status,omitempty"`
 	ContactName    string `json:"contactname,omitempty"`
-	EmergencyPhone int    `json:"emergencyphone,string,omitempty"`
+	EmergencyPhone string `json:"emergencyphone,omitempty"`
 	Relationship   string `json:"relationship,omitempty"`
 	Email          string `json:"email,omitempty"`
 }
@@ -63,6 +63,7 @@ func personalDetails(res http.ResponseWriter, req *http.Request) {
 
 	var js RegisterData
 	msgs := req.FormValue("json")
+	fmt.Println(msgs)
 	err = json.Unmarshal([]byte(msgs), &js)
 	if err != nil {
 		fmt.Println("error:", err)
@@ -71,7 +72,7 @@ func personalDetails(res http.ResponseWriter, req *http.Request) {
 		log.Println(err)
 	}
 
-	q := fmt.Sprintf("insert into personal_details values('%s','%s','%s','%s','%d','%s','%s','%s','%s','%d','%s','%s')", uuid.New(), js.FathersName, js.DOB, js.Gender,
+	q := fmt.Sprintf("insert into personal_details values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')", uuid.New(), js.FathersName, js.DOB, js.Gender,
 		js.Phone, js.Add, js.Nationality, js.Status, js.ContactName, js.EmergencyPhone, js.Relationship, js.Email)
 	_, err = db.Exec(q)
 	if err != nil {
